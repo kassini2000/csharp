981a3450aed1a24e6c673f6e522e2a0824eefd04
Anastasia K <kassini2000@yandex.ru>
2023-10-18 23:50:24 +0300

    выводит четное число

---
+++ task02/Program.cs
@@ -0,0 +1,16 @@
+﻿
+/*
+напишите метод, который на вход принимает число number и выводит, 
+является ли число чётным (делится ли оно на два без остатка).
+*/ 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number % 2) == 0)
+        {
+            Console.WriteLine($"Число `{number}` чётное");
+        }
+        else
+        {
+            Console.WriteLine($"Число `{number}` нечётное");
+        }
