4cb070fc9d3669ebbf73a871f421cde69256a3a0
Anastasia K <kassini2000@yandex.ru>
2023-10-18 23:57:24 +0300

    выводит четные числа от 1 до number

---
+++ task04/Program.cs
@@ -0,0 +1,14 @@
+﻿
+/*
+напишите метод, которая на вход принимает число (number), 
+а на выходе выводит все чётные числа от 1 до number (включительно), 
+разделеные знаком табуляции.
+number = 5 -> 2    4
+number = 8 -> 2    4    6    8
+*/ 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ for (int count = 2; count <= number; count += 2)
+        {
+            Console.Write($"{count}\t");
+        }
