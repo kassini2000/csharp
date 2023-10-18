a6c6d8f7746d42f758cf8828f17639c92c6b1b0b
Anastasia K <kassini2000@yandex.ru>
2023-10-18 23:45:06 +0300

    квадрат числа

---
+++ task00/Program.cs
@@ -0,0 +1,13 @@
+﻿
+/*
+Напишите программу, которая на вход принимает число и выдает
+его квадрат (число умноженное на само себя).
+*/
+
+System.Console.WriteLine("Введите число: ");// быстрый вызов cw enter
+
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = userNumber * userNumber;
+System.Console.WriteLine(result);
+System.Console.WriteLine(userNumber * userNumber);
