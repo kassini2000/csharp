a4edefcf34fc4dfaf1e0ba4001dbbdcf6026ee7e
Anastasia K <kassini2000@yandex.ru>
2023-10-19 00:17:41 +0300

    показывает последнюю цифру из трехзначного числа

---
+++ task07/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+7. Напишите программу, которая принимает на вход трехзначное число и на выходе 
+показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+Console.WriteLine("Введите число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+int result = userNumber1 % 10;
+System.Console.WriteLine(result);
