f8823dfe533a787ef7b41d3929dc22b339aff2fd
Anastasia K <kassini2000@yandex.ru>
2023-10-19 00:14:39 +0300

    максимальное из трех чисел

---
+++ task06/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+максимальное из трех чисел
+*/
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if(number1 > number2 && number1 > number3){
+  max = number1;
+}
+    if(number2 > number1 && number3 > number2){
+      max = number3;
+    }
+    Console.WriteLine(max);
+
