cb94e0aa4caa210fc8fd8726eacd8454d9c682a9
Anastasia K <kassini2000@yandex.ru>
2023-10-19 23:12:33 +0300

    решили задачу 8

---
+++ task08/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+напишите метод, который на вход принимает число number, 
+и выводит является ли число четным.
+number = 4 -> число четное
+number = 7 -> число нечетное
+*/
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 ==0)
+{
+    System.Console.WriteLine($"Число `{number}` четное");
+}
+else
+{
+    System.Console.WriteLine($"Число `{number}` нечетное");
+}
