c7c74973d27085e81c462358a01d273313c08fe7
Anastasia K <kassini2000@yandex.ru>
2023-10-19 22:40:34 +0300

    решение задачи2

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,16 +1,25 @@
 ﻿
 /*
-напишите метод, который на вход принимает число number и выводит, 
-является ли число чётным (делится ли оно на два без остатка).
+найти большее из 2 чисел
+a = 5; b = 7 -> первое число меньше чем второе
+a = 2; b = 2 -> введенные числа равны
+a = -3; b = -9 -> первое число больше второе
 */ 
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
 
-if ((number % 2) == 0)
-        {
-            Console.WriteLine($"Число `{number}` чётное");
-        }
-        else
-        {
-            Console.WriteLine($"Число `{number}` нечётное");
-        }
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < number2)
+{
+     Console.WriteLine($"Первое число `{number1}` меньше чем второе `{number2}`");
+}
+ else if (number2 < number1)
+    {
+        Console.WriteLine($"Первое число `{number1}` больше чем второе `{number2}`");
+    }
+    else if (number2 == number1)
+    {
+        Console.WriteLine($"Введенные числа равны `{number1}`");
+    }
