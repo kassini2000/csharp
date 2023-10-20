8defdefa52bb185fe3b4b25708760a75b83faa79
Anastasia K <kassini2000@yandex.ru>
2023-10-20 22:25:13 +0300

    переименовали папку

---
+++ task14/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+напишите программу, которая на вход принимает число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да
+*/
+
+System.Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ if (number % 7 == 0 && number % 23 == 0)
+ {
+    System.Console.WriteLine("кратно");
+ }
+ else
+ {
+    System.Console.WriteLine("не кратно");
+ }
