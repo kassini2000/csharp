c4ef2b1a7d46662017df5188090d5b2d2f47fa59
Anastasia K <kassini2000@yandex.ru>
2023-10-25 23:07:15 +0300

    решаем задачу 21

---
+++ task21/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Внутри класса Answer напишите метод DistanceBetweenPoints, 
+который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, 
+и возвращает расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7) -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+System.Console.WriteLine("Введите число: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 -z1,2);
+double g = Math.Round(Math.Sqrt(d), 2);
+
+System.Console.WriteLine(g);
