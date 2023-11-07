9186dc60dcb8d6be549e61ef30cd0306f29a4d95
Anastasia K <kassini2000@yandex.ru>
2023-11-07 12:18:26 +0300

    задача30

---
+++ task30/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая выводит массив из 8 
+элементов, заполненный 0 и 1 в случайном порядке.
+*/
+int[] GetRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        size[i] = new Random().Next(0,2);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (i = 0; i < arr.Length; i++)
+    {
+      System.Console.Write($"{arr[i]}, ");  
+    }
+
+}
+int[] userArray = GetRandomArray(9);
+PrintArray(userArray);
