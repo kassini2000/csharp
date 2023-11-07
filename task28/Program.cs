890a127f87cbf1cea900cc73f78a84ec5d7161c5
Anastasia K <kassini2000@yandex.ru>
2023-11-07 11:30:09 +0300

    задача28

---
+++ task28/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход 
+число N и выдает произведение чисел от 1 до N
+*/
+
+int Rrompt(string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int MultNumber(int a)
+{
+    int result = 1;
+    for(int i = 1; i < a; i++)
+    {
+        result = result + i;
+    }
+    return result;
+}
+
+int a = Rrompt("Введите число: ");
+int result = MultNumber(a);
+System.Console.WriteLine(result);
