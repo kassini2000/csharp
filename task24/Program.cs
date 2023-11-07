40c287f7a53dcab75d30102e3f945f96d90904bc
Anastasia K <kassini2000@yandex.ru>
2023-11-07 10:37:43 +0300

    решаем задачу

--- task24/Program.cs
+++ task24/Program.cs
@@ -7,7 +7,7 @@
 8 -> 36
 */
 
-System.Console.Write("Введите число: ");
+/*System.Console.Write("Введите число: ");
 int A = Convert.ToInt32(Console.ReadLine());
 
 int summ = 0;
@@ -16,4 +16,20 @@ for (int i = 1; i <= A; i++)
 {
     summ = summ + i;
 }
-System.Console.WriteLine(summ);
+System.Console.WriteLine(summ);*/
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int Summer(int a)
+{
+    int summ = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+        summ = summ + i;
+    }
+    return summ;
+}
+int userResult = Summer(A);
+System.Console.WriteLine(userResult);
