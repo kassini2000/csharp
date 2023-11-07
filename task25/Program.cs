4e3b5f323af15dbd3ec3cbd7009e03ac55929ec8
Anastasia K <kassini2000@yandex.ru>
2023-11-07 10:59:45 +0300

    решение задачи

---
+++ task25/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу, которая принимает на вход число 
+и выдает количество цифр и число.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.Clear();// для чистой консоли
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+/*int count = 0;
+for(int i = number; i > 0; i /= 10)
+{
+    count = count + 1;
+}*/
+int Result(int number)
+{
+    int count = 0;
+    for (int i = number; i > 0; i /= 10)
+    {
+        count = count + 1;
+    }
+    return count;
+}
+ int userCount = Result(userNumber);
+ System.Console.WriteLine(userCount);
+ 
+
