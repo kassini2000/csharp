785be718cbf2dfc40bf8b3c0d1ad58939bc326d9
Anastasia K <kassini2000@yandex.ru>
2023-11-16 23:47:35 +0300

    решили задачу30

--- task30/Program.cs
+++ task30/Program.cs
@@ -1,24 +1,38 @@
 ﻿/*
-Напишите программу, которая выводит массив из 8 
-элементов, заполненный 0 и 1 в случайном порядке.
+Напишите программу, которая выводит массив 
+из 8 элементов, заполненный 
+0 и 1 в случайном порядке.
 */
-int[] GetRandomArray(int size)
+Console.Clear();
+
+int[] GetRandomArray(int start, int end, int size)
 {
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
-        size[i] = new Random().Next(0,2);
+        array[i] = new Random().Next(size + 1);
     }
     return array;
 }
 
 void PrintArray(int[] arr)
 {
-    for (i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-      System.Console.Write($"{arr[i]}, ");  
+      System.Console.Write(arr[i]);
+      if (i < arr.Length - 1) 
+      System.Console.WriteLine(", "); 
     }
+    PrintArray(arr);
 
-}
-int[] userArray = GetRandomArray(9);
-PrintArray(userArray);
+} 
+System.Console.WriteLine("задай диапазон массива от: ");
+int userStart = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("задай диапазон массива до: ");
+int userEnd = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("задай размер массива до: ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+
+int[] result = GetRandomArray(userStart, userEnd, userSize);
+Console.WriteLine(result);
+
