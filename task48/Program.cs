8dbd132f77d5fca3abdf6eed4d797eec46d1f11c
Anastasia K <kassini2000@yandex.ru>
2023-11-25 20:51:47 +0300

    задача48

---
+++ task48/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
+Aₘₙ = m+n. Выведите полученный массив на экран.
+*/
+pablic static void Main(string[] args)
+{
+    int m = 3;
+    int n = 3;
+    int[,] arr = new int[3, 3];
+    for (int i = 0; i < arr.Length; i++)
+    {
+       arr[i] = new int[3];
+       for (int j = 0; j < arr[i].Length; j++)
+       {
+        arr[i, j] = i +j;
+       }
+    }
+    PrintArr(arr);
+}
+
+void PrintArr(int[,]arr)
+{
+    foreach (var row in arr)
+    {
+        System.Console.WriteLine($"[{strin.Join(" ", row)}]");
+    }
+}
