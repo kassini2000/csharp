9ca43668311d2d83592228b6cd385dbd356b8161
Anastasia K <kassini2000@yandex.ru>
2023-11-29 12:51:43 +0300

    задача67

---
+++ task67/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая будет принимть на вход
+число и возвращать сумму его цифр.
+*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int intN(int num)
+{
+    if(num % 10 > 0)
+     return num % 10 + intN(num / 10);
+    else
+        return 0;
+}
+
+int result = intN(number);
+System.Console.WriteLine(result);
