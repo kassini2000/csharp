2aaa4216b4f9fca3e28e7cf4471a4305351f1e40
Anastasia K <kassini2000@yandex.ru>
2023-11-29 13:26:59 +0300

    задача56

---
+++ task56/Program.cs
@@ -0,0 +1,93 @@
+﻿/*
+Напишите программу для работы с матрицей целых чисел.
+Реализуйте класс Answer, который содержит следующие статические методы:
+SumOfRow(int[,] matrix, int row): Метод для вычисления суммы элементов в заданной строке row матрицы matrix. Метод принимает двумерный массив целых чисел matrix и номер строки row, а возвращает целое число - сумму элементов в данной строке.
+MinimumSumRow(int[,] matrix): Метод для определения строки с наименьшей суммой элементов. Метод принимает двумерный массив целых чисел matrix и возвращает массив из двух элементов: номер строки с наименьшей суммой (нумерация начинается с 0) и саму сумму.
+*/
+using System;
+
+public class Answer
+{
+    public static int SumOfRow(int[,] matrix, int row)
+    {
+        int summ=0;
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            summ = summ + matrix[row,i];
+        }
+        return summ;
+    }
+
+    public static int[] MinimumSumRow(int[,] matrix)
+    {
+        int[] array= new int[]{0,SumOfRow(matrix, 0)};
+        for (int i = 1; i < matrix.GetLength(0); i++)
+        {
+           int summ = SumOfRow(matrix, i);
+           if (array[1]>summ)
+           {
+            array[0]=i;
+            array[1]=summ;
+           }
+        }
+        return array;
+    }
+
+ // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args)
+    {
+        int[,] matrix;
+        if (args.Length == 0)
+        {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+          // Если аргументы не переданы, используем матрицу по умолчанию
+            
+            matrix = new int[,]
+            {
+                {5, 2, 9},
+                {8, 1, 4},
+                {6, 7, 3}
+            };
+        }
+        else
+        {
+            // Иначе, парсим аргументы в двумерный массив
+            string[] rows = args[0].Split(';');
+            matrix = new int[rows.Length, rows[0].Split(',').Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                string[] elements = rows[i].Split(',');
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (int.TryParse(elements[j], out int number))
+                    {
+                        matrix[i, j] = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ошибка при парсинге аргумента {elements[j]}.");
+                        return;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Исходная матрица:");
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+
+        int[] minSumRow = MinimumSumRow(matrix);
+
+        Console.WriteLine($"\nСумма наименьшей строки (строка {minSumRow[0] + 1}): {minSumRow[1]}");
+
+        int rowToSum = 1; 
+        int sum = SumOfRow(matrix, rowToSum);
+        Console.WriteLine($"Сумма элементов в строке {rowToSum + 1}: {sum}");
+    }
+}
