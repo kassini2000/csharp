a7cd1c74a1af41bc7bbfb0d2a4d70d4c133facb9
Anastasia K <kassini2000@yandex.ru>
2023-11-29 12:11:47 +0300

    Задача65

---
+++ task65/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задайте значение M и N. Напишите программу, 
+которая выдает все натуральные числа от M до N.
+*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число: ");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ведите число: ");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+
+void intN(int first, int last)
+{
+    if(last >= first)
+    {
+        System.Console.Write(first + " ");
+        intN(first + 1, last);
+    }
+    else
+       return;
+}
+
+intN(namber1, namber2);
+
